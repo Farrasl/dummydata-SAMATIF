0ceb4c96b375482263ffbc10a7d6bc509b0d3c2f
farras <farraslathief@gmail.com>
2024-06-17 00:19:30 +0700

    fifth commit

---
+++ api/hello.php.js
@@ -0,0 +1,22 @@
+// api/hello.php.js
+
+module.exports = (req, res) => {
+    const { spawn } = require('child_process');
+    const php = spawn('php', ['index.php']);
+  
+    php.stdout.on('data', (data) => {
+      res.status(200).send(data.toString());
+    });
+  
+    php.stderr.on('data', (data) => {
+      res.status(500).send(data.toString());
+    });
+  
+    php.on('close', (code) => {
+      if (code !== 0) {
+        console.error(`PHP script exited with code ${code}`);
+        res.status(500).end();
+      }
+    });
+  };
+  
